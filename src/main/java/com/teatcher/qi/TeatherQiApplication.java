62dbfc2dde109472529ea7ebb118352b5d78c8f6
ewing <18381692672@163.com>
2024-06-17 22:25:56 +0800

    初始项目push

---
+++ src/main/java/com/teatcher/qi/TeatherQiApplication.java
@@ -0,0 +1,13 @@
+package com.teatcher.qi;
+
+import  org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeatherQiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeatherQiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teatcher/qi/TeatherQiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teatcher.qi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeatherQiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
